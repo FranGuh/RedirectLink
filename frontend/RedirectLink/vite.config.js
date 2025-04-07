2036c52323e247df6131b20492c4d964acdc7710
Gustavo Francisco <franguho@gmail.com>
2025-04-06 18:40:04 -0600

    Vercel

--- frontend/RedirectLink/vite.config.js
+++ frontend/RedirectLink/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Asegura que las rutas sean correctas en producción
+  build: {
+    outDir: 'dist' // Asegura que la salida del build esté en la carpeta dist
+  }
 })
